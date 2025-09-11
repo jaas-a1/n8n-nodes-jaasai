8d8976f3b41838d17fdd67d121af54cc67cbc228
hlardiez <hlardiez@msn.com>
2025-09-10 22:44:50 -0400

    10032025 2244

--- nodes/jaasai/Jaasai.node.ts
+++ nodes/jaasai/Jaasai.node.ts
@@ -125,89 +125,88 @@ export class Jaasai implements INodeType {
 
 
   async execute(this: IExecuteFunctions): Promise<INodeExecutionData[][]> {
-    const items = this.getInputData();
+    //const items = this.getInputData();
     const returnData: INodeExecutionData[] = [];
-    for (let i = 0; i < items.length; i++) {
-      try {
-        //const city = this.getNodeParameter('city', i) as string;
-        const question = this.getNodeParameter('question',i)||"";//items[i].json.question as string||"";
-				const answer  = this.getNodeParameter('answer',i)|| "";//items[i].json.answer as string||"";
-				const context = this.getNodeParameter('context',i)||"";//items[i].json.context as string||"";
-				const ground_truth_answer = this.getNodeParameter('ground_truth_answer',i)||"";//items[i].json.ground_truth_answer as string||"";
-				const cohort = this.getNodeParameter('cohort',i)||"N8N-NC";//items[i].json.cohort as string||"N8N-NC";
+    try {
+      //const city = this.getNodeParameter('city', i) as string;
+      const question = this.getNodeParameter('question',0)||"";//items[i].json.question as string||"";
+			const answer  = this.getNodeParameter('answer',0)|| "";//items[i].json.answer as string||"";
+			const context = this.getNodeParameter('context',0)||"";//items[i].json.context as string||"";
+			const ground_truth_answer = this.getNodeParameter('ground_truth_answer',0)||"";//items[i].json.ground_truth_answer as string||"";
+			const cohort = this.getNodeParameter('cohort',0)||"N8N-NC";//items[i].json.cohort as string||"N8N-NC";
 
-				const evaluation_criteria1 = this.getNodeParameter('criteria1', i) as string ||"Accuracy";
-				const evaluation_criteria2 = this.getNodeParameter('criteria2', i) as string ||"";
-				const evaluation_criteria3 = this.getNodeParameter('criteria3', i) as string ||"";
-				const evaluation_criteria4 = this.getNodeParameter('criteria4', i) as string ||"";
-				const evaluation_criteria5 = this.getNodeParameter('criteria5', i) as string ||"";
+			const evaluation_criteria1 = this.getNodeParameter('criteria1', 0) as string ||"Accuracy";
+			const evaluation_criteria2 = this.getNodeParameter('criteria2', 0) as string ||"";
+			const evaluation_criteria3 = this.getNodeParameter('criteria3', 0) as string ||"";
+			const evaluation_criteria4 = this.getNodeParameter('criteria4', 0) as string ||"";
+			const evaluation_criteria5 = this.getNodeParameter('criteria5', 0) as string ||"";
 
 
-				const evaluation_criteria = [evaluation_criteria1, evaluation_criteria2, evaluation_criteria3, evaluation_criteria4, evaluation_criteria5]
+			const evaluation_criteria = [evaluation_criteria1, evaluation_criteria2, evaluation_criteria3, evaluation_criteria4, evaluation_criteria5]
     .filter(c => c && c.trim() !== "");
 
 
-				const type = this.getNodeParameter('type', i) as string;
+			const type = this.getNodeParameter('type', 0) as string;
 
 				//const condition = this.getNodeParameter('condition', i) as string;
         //const threshold = this.getNodeParameter('threshold', i) as number;
-        const credentials = await this.getCredentials('jaasaiApi');
-        const apiKey = credentials.apiKey as string;
-				if (apiKey === undefined) {
-					throw new NodeOperationError(this.getNode(), 'No API key provided!');
-				}
-				//console.log('API Key:', apiKey);
-				// Fetch the data from the JaaS AP
-				const jaasData = await this.helpers.httpRequestWithAuthentication.call(
-					this,
-					'jaasaiApi', // credential name
-					{
-          	method: 'POST',
-          	//url: `http://api.jaas-ai.com/v1/evaluate`,
-						url: 'https://api.jaas-ai.com/v1/evaluate',
+      const credentials = await this.getCredentials('jaasaiApi');
+      const apiKey = credentials.apiKey as string;
+			if (apiKey === undefined) {
+				throw new NodeOperationError(this.getNode(), 'No API key provided!');
+			}
+			//console.log('API Key:', apiKey);
+			// Fetch the data from the JaaS AP
+			const jaasData = await this.helpers.httpRequestWithAuthentication.call(
+				this,
+				'jaasaiApi', // credential name
+				{
+         	method: 'POST',
+         	//url: `http://api.jaas-ai.com/v1/evaluate`,
+					url: 'https://api.jaas-ai.com/v1/evaluate',
 
-						headers: {
-							'accept': 'application/json',
-							'Authorization': 'Bearer ' + apiKey,
-						},
-						body: {
-							'question': question,
-							'ground_truth_answer': ground_truth_answer,
-							'answer': answer,
-							'context': context,
-							//other parameters,
-							'evaluation_criteria': evaluation_criteria, // or use the condition variable
-							'type': type, // 'S' for single, 'D' for conversational, 'V' for verified
-							'cohort': cohort, // Cohort name, e.g., 'N8N-NC'-> Default
+				headers: {
+						'accept': 'application/json',
+						'Authorization': 'Bearer ' + apiKey,
+					},
+					body: {
+						'question': question,
+						'ground_truth_answer': ground_truth_answer,
+						'answer': answer,
+						'context': context,
+						//other parameters,
+						'evaluation_criteria': evaluation_criteria, // or use the condition variable
+						'type': type, // 'S' for single, 'D' for conversational, 'V' for verified
+						'cohort': cohort, // Cohort name, e.g., 'N8N-NC'-> Default
 
-          	},
-          	json: true,
-      	  }
-				);
+        	},
+         	json: true,
+     	  }
+			);
 
 
+       returnData.push({
+         json: {
+          'status': jaasData.status,
+					'criteria': jaasData.criteria,
+					'type': jaasData.evaluation_type,
+         },
+       });
+    } catch (error) {
+      if (this.continueOnFail()) {
         returnData.push({
           json: {
-            'status': jaasData.status,
-						'criteria': jaasData.criteria,
-						'type': jaasData.evaluation_type,
+            error: error.message,
           },
+					pairedItem: {
+ 						item: 0,
+					},
         });
-      } catch (error) {
-        if (this.continueOnFail()) {
-          returnData.push({
-            json: {
-              error: error.message,
-            },
-						pairedItem: {
-    						item: i,
-						},
-          });
-          continue;
-        }
-        throw new NodeOperationError(this.getNode(), error);
+        //continue;
       }
+      throw new NodeOperationError(this.getNode(), error);
     }
+
     return [returnData];
   }
 }
